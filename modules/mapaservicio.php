6c51a840f7ab35a5008a7b7d8eba77b1b8ef5994
lfernandortiz <lfernandortiz@gmail.com>
2017-04-17 16:42:53 -0500

    pruebas de cross domain para curl php

--- modules/mapaservicio.php
+++ modules/mapaservicio.php
@@ -1,15 +1,16 @@
 <?php      
       header("Access-Control-Allow-Origin: *");
-       $datajson = "";
+       
        $dirWS = 'http://dromedicas.ddns.net:8080/serviciomapasfarmanorte/rest/sucursales';      
        $ch = curl_init($dirWS);
        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-          'Content-Type: application/json')
-       );
-       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+       //    'Content-Type: application/json')
+       // );
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false)
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       
        curl_setopt($ch, CURLOPT_TIMEOUT, 40);
        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 40);
        //execute post
@@ -18,7 +19,7 @@
       //close connection
        curl_close($ch);
        echo $jsonResponse;
-       // echo $jsonResponse;
 
-      
+
+     
 ?>
